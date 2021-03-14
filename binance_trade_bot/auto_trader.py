cb30875e531620edba5c0193fbfc5a5a3dbb7e72
Ryan Sonshine <sonshine@amazon.com>
2021-03-14 15:46:25 -0400

    Add Binance websocket support
    Squashed as some commits were broken by the rebase:
    
    Add stream manager class
    
    Remove all_tickers
    
    Add method for starting all mark price socket
    
    Add handler for closing websockets on exit
    
    whitespace refactor
    
    Refactor naming and move sleep timer
    
    Update connect_args
    
    Disable check for same thread
    
    Add initial implementation of websockets for getting all mark prices
    
    Co-authored-by: James Waters <james@jcwaters.co.uk>

--- binance_trade_bot/auto_trader.py
+++ binance_trade_bot/auto_trader.py
@@ -10,7 +10,6 @@ from .config import Config
 from .database import Database
 from .logger import Logger
 from .models import Coin, CoinValue, Pair
-from .utils import get_market_ticker_price_from_list
 
 
 class AutoTrader:
@@ -20,7 +19,7 @@ class AutoTrader:
         self.logger = logger
         self.config = config
 
-    def transaction_through_bridge(self, pair: Pair, all_tickers):
+    def transaction_through_bridge(self, pair: Pair):
         """
         Jump from the source coin to the destination coin through bridge coin
         """
@@ -31,12 +30,12 @@ class AutoTrader:
         # This'll do for now
         result = None
         while result is None:
-            result = self.manager.buy_alt(pair.to_coin, self.config.BRIDGE, all_tickers)
+            result = self.manager.buy_alt(pair.to_coin, self.config.BRIDGE)
 
         self.db.set_current_coin(pair.to_coin)
-        self.update_trade_threshold(float(result["price"]), all_tickers)
+        self.update_trade_threshold(float(result["price"]))
 
-    def update_trade_threshold(self, current_coin_price: float, all_tickers):
+    def update_trade_threshold(self, current_coin_price: float):
         """
         Update all the coins with the threshold of buying the current held coin
         """
@@ -49,7 +48,7 @@ class AutoTrader:
         session: Session
         with self.db.db_session() as session:
             for pair in session.query(Pair).filter(Pair.to_coin == current_coin):
-                from_coin_price = get_market_ticker_price_from_list(all_tickers, pair.from_coin + self.config.BRIDGE)
+                from_coin_price = self.manager.get_ticker_price(pair.from_coin + self.config.BRIDGE)
 
                 if from_coin_price is None:
                     self.logger.info(
@@ -63,8 +62,6 @@ class AutoTrader:
         """
         Initialize the buying threshold of all the coins for trading between them
         """
-        all_tickers = self.manager.get_all_market_tickers()
-
         session: Session
         with self.db.db_session() as session:
             for pair in session.query(Pair).filter(Pair.ratio.is_(None)).all():
@@ -72,14 +69,14 @@ class AutoTrader:
                     continue
                 self.logger.info(f"Initializing {pair.from_coin} vs {pair.to_coin}", False)
 
-                from_coin_price = get_market_ticker_price_from_list(all_tickers, pair.from_coin + self.config.BRIDGE)
+                from_coin_price = self.manager.get_ticker_price(pair.from_coin + self.config.BRIDGE)
                 if from_coin_price is None:
                     self.logger.info(
                         "Skipping initializing {}, symbol not found".format(pair.from_coin + self.config.BRIDGE)
                     )
                     continue
 
-                to_coin_price = get_market_ticker_price_from_list(all_tickers, pair.to_coin + self.config.BRIDGE)
+                to_coin_price = self.manager.get_ticker_price(pair.to_coin + self.config.BRIDGE)
                 if to_coin_price is None:
                     self.logger.info(
                         "Skipping initializing {}, symbol not found".format(pair.to_coin + self.config.BRIDGE)
@@ -107,15 +104,13 @@ class AutoTrader:
             if self.config.CURRENT_COIN_SYMBOL == "":
                 current_coin = self.db.get_current_coin()
                 self.logger.info(f"Purchasing {current_coin} to begin trading")
-                all_tickers = self.manager.get_all_market_tickers()
-                self.manager.buy_alt(current_coin, self.config.BRIDGE, all_tickers)
+                self.manager.buy_alt(current_coin, self.config.BRIDGE)
                 self.logger.info("Ready to start trading")
 
     def scout(self):
         """
         Scout for potential jumps from the current coin to another coin
         """
-        all_tickers = self.manager.get_all_market_tickers()
 
         current_coin = self.db.get_current_coin()
         # Display on the console, the current coin+Bridge, so users can see *some* activity and not think the bot has
@@ -128,7 +123,7 @@ class AutoTrader:
             end="\r",
         )
 
-        current_coin_price = get_market_ticker_price_from_list(all_tickers, current_coin + self.config.BRIDGE)
+        current_coin_price = self.manager.get_ticker_price(current_coin + self.config.BRIDGE)
 
         if current_coin_price is None:
             self.logger.info("Skipping scouting... current coin {} not found".format(current_coin + self.config.BRIDGE))
@@ -139,7 +134,7 @@ class AutoTrader:
         for pair in self.db.get_pairs_from(current_coin):
             if not pair.to_coin.enabled:
                 continue
-            optional_coin_price = get_market_ticker_price_from_list(all_tickers, pair.to_coin + self.config.BRIDGE)
+            optional_coin_price = self.manager.get_ticker_price(pair.to_coin + self.config.BRIDGE)
 
             if optional_coin_price is None:
                 self.logger.info(
@@ -168,14 +163,12 @@ class AutoTrader:
         if ratio_dict:
             best_pair = max(ratio_dict, key=ratio_dict.get)
             self.logger.info(f"Will be jumping from {current_coin} to {best_pair.to_coin_id}")
-            self.transaction_through_bridge(best_pair, all_tickers)
+            self.transaction_through_bridge(best_pair)
 
     def update_values(self):
         """
         Log current value state of all altcoin balances against BTC and USDT in DB.
         """
-        all_ticker_values = self.manager.get_all_market_tickers()
-
         now = datetime.now()
 
         session: Session
@@ -185,8 +178,8 @@ class AutoTrader:
                 balance = self.manager.get_currency_balance(coin.symbol)
                 if balance == 0:
                     continue
-                usd_value = get_market_ticker_price_from_list(all_ticker_values, coin + "USDT")
-                btc_value = get_market_ticker_price_from_list(all_ticker_values, coin + "BTC")
+                usd_value = self.manager.get_ticker_price(coin + "USDT")
+                btc_value = self.manager.get_ticker_price(coin + "BTC")
                 cv = CoinValue(coin, balance, usd_value, btc_value, datetime=now)
                 session.add(cv)
                 self.db.send_update(cv)
